79e43c6c0d182b24136bb616537ed2c4512df7d1
Ryoma Kai <706834+legnoh@users.noreply.github.com>
2023-03-05 16:46:51 +0900

    change out of codeowner approve

--- modules/repo/main.tf
+++ modules/repo/main.tf
@@ -63,7 +63,6 @@ resource "github_branch_protection" "main" {
 
   required_pull_request_reviews {
     dismiss_stale_reviews           = true
-    require_code_owner_reviews      = true
     required_approving_review_count = 1
   }
   
